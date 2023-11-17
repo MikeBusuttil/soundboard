c1fbcca3b6cb1361a4d5bafd4a2980a4755a2ebe
Mike Busuttil <mikebusuttil@gmail.com>
2023-11-16 22:53:40 -0500

    SB-2 - get em all in here

---
+++ ui/scripts/import-all-fx.py
@@ -0,0 +1,20 @@
+import os
+
+fx_foler = os.path.dirname(os.path.abspath(__file__)) + "/../src/fx"
+
+index_js = ["module.exports = {"]
+
+def prop(file):
+    return file[:-4].replace("'", '')
+
+def file_name(file):
+    return file.replace("'", "\\'")
+
+for file in os.listdir(fx_foler):
+    if file.endswith(".mp3"):
+        index_js.append(f"  '{prop(file)}': require('./{file_name(file)}'),")
+
+index_js.append("}\n")
+
+with open(fx_foler + "/index.js", "w") as f:
+    f.write("\n".join(index_js))
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,8 @@
 import dynamite from "./fx/Boom goes the dynamite.mp3"
 import greatSuccess from "./fx/Borat Great Success.mp3"
 import DJ from "./fx/DJ Airhorn Sound Effect.mp3"
+import * as sounds from './fx'
+// import every MP3 from the fx directory
 
 import "./App.css"
 
@@ -10,11 +12,19 @@ const fireSound = (effect) => {
 }
 
 function App() {
+  console.log(sounds)
   return (
     <div className="App">
-      <button onClick={() => fireSound(dynamite)} className="absolute top-20 left-20">🧨</button>
-      <button onClick={() => fireSound(greatSuccess)} className="absolute top-20 right-20">👍👍</button>
+      <button onClick={() => fireSound(dynamite)} className="absolute top-20 left-20">🧨boom goes the dynamite🧨</button>
+      <button onClick={() => fireSound(greatSuccess)} className="absolute top-20 right-20">👍great success👍</button>
+      <button onClick={() => fireSound(greatSuccess)} className="absolute top-20 right-20">👍great success👍</button>
       <button onClick={() => fireSound(DJ)} className="absolute bottom-20 right-20">📯</button>
+
+      <div className="h-28"/>
+      {Object.entries(sounds).map(([name, effect]) => (
+        <button key={name} onClick={() => fireSound(effect)} className="m-1 p-1 border text-sm border-black rounded">{name}</button>
+      ))
+      }
     </div>
   )
 }
