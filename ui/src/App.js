d1e06c44e6eba5c24ff3e7c089924b32d0d42361
Mike Busuttil <mikebusuttil@gmail.com>
2023-11-16 22:17:55 -0500

    SB-4 - unlimited playback

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,20 +1,20 @@
-import { useState } from "react"
-
 import dynamite from "./fx/Boom goes the dynamite.mp3"
 import greatSuccess from "./fx/Borat Great Success.mp3"
 import DJ from "./fx/DJ Airhorn Sound Effect.mp3"
 
 import "./App.css"
 
+const fireSound = (effect) => {
+  const audio = new Audio(effect)
+  audio.play()
+}
+
 function App() {
-  const [audio1] = useState(new Audio(dynamite))
-  const [audio2] = useState(new Audio(greatSuccess))
-  const [audio3] = useState(new Audio(DJ))
   return (
     <div className="App">
-      <button onClick={() => audio1.play()}>🧨</button>
-      <button onClick={() => audio2.play()}>👍👍</button>
-      <button onClick={() => audio3.play()}>📯</button>
+      <button onClick={() => fireSound(dynamite)} className="absolute top-20 left-20">🧨</button>
+      <button onClick={() => fireSound(greatSuccess)} className="absolute top-20 right-20">👍👍</button>
+      <button onClick={() => fireSound(DJ)} className="absolute bottom-20 right-20">📯</button>
     </div>
   )
 }
